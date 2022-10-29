713d28a4bcc6984932d00fddb02fe580ec75fb59
baruch yakubov <baruchyaku@gmail.com>
2022-10-29 20:00:27 +0300

    some changes

--- js/services/meme-service.js
+++ js/services/meme-service.js
@@ -1,5 +1,4 @@
 'use strict'
-var gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
 var gImgs = [{ id: 1, url: 'img/1.jpg', keywords: ['funny', 'aqward'] }, { id: 2, url: 'img/2.jpg', keywords: ['cute', 'animal'] }, { id: 3, url: 'img/3.jpg', keywords: ['cute', 'animal'] },
 { id: 4, url: 'img/4.jpg', keywords: ['cute', 'animal'] }, { id: 5, url: 'img/5.jpg', keywords: ['cute', 'funny'] }, { id: 6, url: 'img/6.jpg', keywords: ['funny', 'bad'] },
 { id: 7, url: 'img/7.jpg', keywords: ['funny', 'cute'] }, { id: 8, url: 'img/8.jpg', keywords: ['funny', 'bad'] }, { id: 9, url: 'img/9.jpg', keywords: ['funny', 'cute'] }
